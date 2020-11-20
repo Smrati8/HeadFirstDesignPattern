c3f874baf7e699b9f1b9ff4ee5e48949ff7ef2ea
Smrati8 <smratipandey7@gmail.com>
2020-11-19 21:22:40 -0500

    Commit: Decoy Duck Class is added

--- src/Duck.java
+++ src/Duck.java
@@ -1,8 +1,10 @@
 public abstract class Duck {
     public void quack(){
+        System.out.println("Quack");
     }
 
     public void swim() {
+        System.out.println("Swim");
     }
 
     public void fly() {
