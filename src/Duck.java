546d708cea6db27f2714b5bde0d42e775118a290
Smrati8 <smratipandey7@gmail.com>
2020-11-19 20:27:11 -0500

    Initial Commit: Basic Structure and start of classes

---
+++ src/Duck.java
@@ -0,0 +1,11 @@
+public abstract class Duck {
+    public void quack(){
+        System.out.println("Quack");
+    }
+
+    public void swim() {
+        System.out.println("Swim");
+    }
+
+    abstract public void display();
+}
