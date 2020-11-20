919dffd69324020869a9e301d5dbeff1bfcedf36
Smrati8 <smratipandey7@gmail.com>
2020-11-20 12:00:24 -0500

    Commit: Added Setter Methods

--- src/ch1DesignPattern/Duck.java
+++ src/ch1DesignPattern/Duck.java
@@ -17,4 +17,12 @@ public abstract class Duck {
     }
 
     abstract public void display();
+
+    public void setFlyBehavior(FlyBehavior fb){
+        this.flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb){
+        this.quackBehavior = qb;
+    }
 }
--- src/ch1DesignPattern/MiniDuckSimulator.java
+++ src/ch1DesignPattern/MiniDuckSimulator.java
@@ -5,5 +5,11 @@ public class MiniDuckSimulator {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.performQuack();
+        model.setFlyBehavior(new FlyWithWings());
+        model.performFly();
     }
 }
